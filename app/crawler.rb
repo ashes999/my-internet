bd1d2fed85dcc2587cbdd980dc42054010cbaf25
ashes999 <ashes999@yahoo.com>
2014-11-11 12:38:11 -0500

    Added infrastructure for crawler thread

---
+++ app/crawler.rb
@@ -0,0 +1,7 @@
+class Crawler
+  def run(server)
+    while (!server.exit) do
+      sleep(1)
+    end
+  end
+end
--- app/server.rb
+++ app/server.rb
@@ -2,8 +2,12 @@ class Server
 
   PORT = 9292
   include Rack
+  require './app/crawler'
+  
+  attr_reader :exit
   
   def run
+    @exit = false
     app = Builder.new do 
       use Rewrite do
         rewrite '/', '/index.html'
@@ -17,10 +21,21 @@ class Server
       # Handles static content
       run Directory.new(Dir.pwd)
     end
+    
+    crawler = Thread.new {
+      # TODO: replace with DTO for data to share
+      Crawler.new.run(self)
+    }
 
     Database.create_if_missing()
     puts "Serving static content from #{Dir.pwd}"
 
     Handler::Thin.run app, :Port => PORT
+    
+    @exit = true
+    puts 'Waiting for crawler to terminate ...'
+    crawler.join
+    
+    puts 'Bye!'
   end
 end
